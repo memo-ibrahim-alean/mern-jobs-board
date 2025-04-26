875f2c4bf9bd129bd932b26460f69b6d01832589
memo-ibrahim-alean <mohamed.ibrahim.alean@gmail.com>
2025-04-27 02:56:04 +0300

    Connect server to MongoDB using Mongoose

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
 
@@ -10,3 +11,8 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.send("Jobs API is running!");
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
