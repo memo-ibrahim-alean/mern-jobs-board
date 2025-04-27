a690bc14591701ec8be4895c6c96427ca5598025
memo-ibrahim-alean <mohamed.ibrahim.alean@gmail.com>
2025-04-27 07:04:21 +0300

    connect backend to frontend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import Job from "./models/Job.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -25,24 +27,44 @@ app.get("/", (req, res) => {
 
 app.get("/jobs", async (req, res) => {
   try {
-    const jobs = await Job.find();
-    console.log("Jobs fetched successfully:", jobs);
+    const limit = parseInt(req.query._limit);
+    let jobs = await Job.find();
+
+    if (!isNaN(limit)) {
+      jobs = jobs.slice(0, limit);
+    }
+
     res.status(200).json(jobs);
   } catch (error) {
-    console.error("Error fetching jobs:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.get("/jobs/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const job = await Job.findById(id);
+    if (!job) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+    console.log("Job fetched successfully:", job);
+    res.status(200).json(job);
+  } catch (error) {
+    console.error("Error fetching job:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
 
 app.post("/jobs", async (req, res) => {
-  console.log("Received job data:", req.body);
+  // console.log("POST request received at /jobs");
+  // console.log("Received job data:", req.body);
   try {
     const newJob = new Job(req.body);
     const savedJob = await newJob.save();
-    console.log("Job saved successfully:", savedJob);
+    // console.log("Job saved successfully:", savedJob);
     res.status(201).json(savedJob);
   } catch (error) {
-    console.error("Error saving job:", error);
+    // console.error("Error saving job:", error);
     res.status(400).json({ message: "Error creating job", error });
   }
 });
@@ -57,10 +79,10 @@ app.put("/jobs/:id", async (req, res) => {
     if (!updatedJob) {
       return res.status(404).json({ message: "Job not found" });
     }
-    console.log("Job updated successfully:", updatedJob);
+    // console.log("Job updated successfully:", updatedJob);
     res.status(200).json(updatedJob);
   } catch (error) {
-    console.error("Error updating job:", error);
+    // console.error("Error updating job:", error);
     res.status(400).json({ message: "Error updating job", error });
   }
 });
@@ -72,10 +94,10 @@ app.delete("/jobs/:id", async (req, res) => {
     if (!deletedJob) {
       return res.status(404).json({ message: "Job not found" });
     }
-    console.log("Job deleted successfully:", deletedJob);
+    // console.log("Job deleted successfully:", deletedJob);
     res.status(200).json({ message: "Job deleted successfully" });
   } catch (error) {
-    console.error("Error deleting job:", error);
+    // console.error("Error deleting job:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
--- server/models/Job.js
+++ server/models/Job.js
@@ -6,59 +6,39 @@ const jobsSchema = new Schema({
   title: {
     type: String,
     required: true,
-    trim: true,
-    minlength: 3,
-    maxlength: 100,
   },
   type: {
     type: String,
     required: true,
-    enum: ["Full-Time", "Part-Time"],
   },
   description: {
     type: String,
     required: false,
-    trim: true,
-    maxlength: 500,
   },
   location: {
     type: String,
     required: true,
-    trim: true,
-    minlength: 2,
-    maxlength: 100,
   },
   salary: {
     type: String,
     required: false,
-    trim: true,
-    enum: ["$60K - $70K", "$70K - $80K", "$90K - $100K", "$100K - $110K"], // بناءً على القيم في jobs.json
   },
   company: {
     name: {
       type: String,
       required: true,
-      trim: true,
-      minlength: 2,
-      maxlength: 100,
     },
     description: {
       type: String,
       required: false,
-      trim: true,
-      maxlength: 500,
     },
     contactEmail: {
       type: String,
       required: false,
-      trim: true,
-      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
     },
     contactPhone: {
       type: String,
       required: false,
-      trim: true,
-      // match: [/^\+?[1-9]\d{1,14}$/, "Please enter a valid phone number"],
     },
   },
   createdAt: {
