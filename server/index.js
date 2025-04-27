e24059b1e41f6e7ae9f815bd687476b1dd17b810
memo-ibrahim-alean <mohamed.ibrahim.alean@gmail.com>
2025-04-27 03:58:23 +0300

    Added GET /jobs endpoint to retrieve all job listings from MongoDB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import Job from "./models/Job.js"; // أضفت السطر ده
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -18,4 +21,15 @@ app.listen(PORT, () => {
 
 app.get("/", (req, res) => {
   res.send("Jobs API is running!");
+});
+
+app.get("/jobs", async (req, res) => {
+  try {
+    const jobs = await Job.find();
+    console.log("Jobs fetched successfully:", jobs);
+    res.status(200).json(jobs);
+  } catch (error) {
+    console.error("Error fetching jobs:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
