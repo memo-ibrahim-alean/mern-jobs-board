28c1c0756a6b7037ac43fb6e17dc3db5c7d2df06
memo-ibrahim-alean <mohamed.ibrahim.alean@gmail.com>
2025-04-27 07:54:57 +0300

    update cors on index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://react-jobs-board-ten.vercel.app/"], 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+}));
 
 mongoose
   .connect(process.env.MONGO_URI)
