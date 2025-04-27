0046ac6c352b095e303bea5e24e17e00d14e1a49
memo-ibrahim-alean <mohamed.ibrahim.alean@gmail.com>
2025-04-27 03:10:35 +0300

    Add Job schema and model for MongoDB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
@@ -11,8 +19,3 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.send("Jobs API is running!");
 });
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
---
+++ server/models/Job.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const jobsSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Job = mongoose.model("Job", jobsSchema);
+
+export default Job;
